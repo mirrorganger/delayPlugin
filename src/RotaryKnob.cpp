19ffc4d8fe50437a992a95cb386a6dadb131b7ed
mirrorganger <cesargufer@gmail.com>
2024-10-02 22:36:12 +0200

    Added rotary knob class

---
+++ src/RotaryKnob.cpp
@@ -0,0 +1,29 @@
+#include "RotaryKnob.h"
+
+namespace delay_plugin
+{
+
+    RotaryKnob::RotaryKnob(const juce::String &name,
+                           juce::AudioProcessorValueTreeState &vts,
+                           const juce::ParameterID &paramId,
+                           const Size & size)
+        : _attachment(vts, paramId.getParamID(),_slider) 
+
+    {
+        _slider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
+        _slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 16);
+        _slider.setBounds(0, 0, 70, 86);
+        addAndMakeVisible(_slider);
+        _label.setText(name, juce::NotificationType::dontSendNotification);
+        _label.setJustificationType(juce::Justification::horizontallyCentred);
+        _label.setBorderSize(juce::BorderSize<int>(0, 0, 2, 0));
+        _label.attachToComponent(&_slider, false);
+        addAndMakeVisible(_label);
+        setSize(size.width, size.height);
+    }
+
+    void RotaryKnob::resized(){
+    _slider.setTopLeftPosition(0,24);
+    }
+
+}
--- src/RotaryKnob.h
+++ src/RotaryKnob.h
@@ -2,21 +2,29 @@
 
 #include <juce_audio_processors/juce_audio_processors.h>
 
-namespace delay_pluygin
+namespace delay_plugin
 {
 
 struct RotaryKnob : public juce::Component
 {
+  struct Size{
+    int width;
+    int height;
+  };
+
   RotaryKnob(const juce::String& name, 
              juce::AudioProcessorValueTreeState& vts,
-             const juce::ParameterID & paramId);
+             const juce::ParameterID & paramId,
+             const Size& size);
   RotaryKnob (const RotaryKnob&) = delete;
   RotaryKnob& operator= (const RotaryKnob&) = delete;
+  ~RotaryKnob() override = default;
 
+  void resized() override;
 
   juce::Slider _slider;
   juce::Label _label;
-  juce::AudioProcessorValueTreeState _attachment;
+  juce::AudioProcessorValueTreeState::SliderAttachment _attachment;
 };
 
 }
